0b410d4a1684d65c717a72e8ea1dad3d2597fd67
Gabriel Lucas da Silva Duarte - 32215799 <32215799@ulife.com.br>
2023-11-13 11:32:35 -0300

    angular

--- src/app/componentes/list-render/list-render.component.ts
+++ src/app/componentes/list-render/list-render.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-
+  animais = [
+    {nome:"Larry", tipo:"cachorro"},
+    {nome:"Miau", tipo:"gato"},
+    {nome:"Anita", tipo:"cachorro"},
+    {nome:"Litajara", tipo:"cavalo"},
+    
+  ]
 }
